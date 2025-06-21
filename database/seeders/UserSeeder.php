3db7c404cf61db620f3c0a233e39e4801c3b473b
Burak Kaynar <60446917+bkaynar@users.noreply.github.com>
2025-06-21 18:11:00 +0300

    fix: Admin rolü silme işlemi eklendi, rollerin atanma yöntemi güncellendi

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,6 +11,9 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // Eski büyük harfli admin rolünü sil
+        \Spatie\Permission\Models\Role::where('name', 'Admin')->delete();
+
         // Roller yoksa oluştur
         $adminRole = Role::firstOrCreate(['name' => 'admin']);
         $userRole  = Role::firstOrCreate(['name' => 'user']);
@@ -25,7 +28,8 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
-        $admin->assignRole($adminRole);
+        // Tüm rollerini kaldırıp sadece admin rolünü ata
+        $admin->syncRoles([$adminRole]);
 
         // Normal kullanıcı
         $user = User::firstOrCreate(
@@ -37,6 +41,6 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
-        $user->assignRole($userRole);
+        $user->syncRoles([$userRole]);
     }
 }
